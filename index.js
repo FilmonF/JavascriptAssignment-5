1ad0268c9308a619bddb955872c90ab522475146
FilmonF <filmonfirezgi@gmail.com>
2024-10-21 19:46:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// function add(a.b){ 
+//     return a+b; 
+// } 
+//  Arrow form of the above function is the following 
+ 
+let add = (a, b) =>  a+b 
+ 
+console.log(add(5,12)) 
+
+// 2. function multiply (a,b) { 
+//     return a*b;  
+// } 
+// Arrow form of the above function is the following: 
+
+let multiply = (a,b) => a*b  
+
+console.log(multiply(a,b)) 
+
+// function greeet (name){ 
+//     return 'Hello, ' + name; 
+} 
+// Arrow form of the above function is the following 
+
+
+let greet = name => console.log(`Hello ${name}`)
